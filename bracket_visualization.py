c29c20be664fe8d2a65a76700bf70f231834531f
Luciana Solorzano <luciana@brightvessel.com>
2024-04-16 13:44:44 -0400

    bracket vis Luciana

--- bracket_visualization.py
+++ bracket_visualization.py
@@ -1,5 +1,6 @@
 # import bracket 
 import populate_dictionaries
+import subprocess
 
 # # Define the tournament structure
 # team = [
@@ -49,17 +50,20 @@ def generate_bracket(participants):
     return bracket
 
 
-def print_bracket(bracket, team_east):
+def print_bracket(bracket, winners):
+    #counter for winners: 
+    j = 0
     for i, round_matches in enumerate(bracket):
         print(f"Round {i + 1}:")
         for match in round_matches:
             player1 = match[0] if match[0] else 'TBD'
             player2 = match[1] if match[1] else 'TBD'
             winner = ""
-            if team_east[player1] > team_east[player2]:
+            if player1 == winners[j]:
                 winner = player1
             else:
                 winner = player2
+            j = j + 1
             max_length = max(len(player1), len(player2))
             print(f"    {player1:<{max_length}}\t──┐")
             print(f"    {' ':<{max_length}}\t  │-- " + winner) 
@@ -67,19 +71,32 @@ def print_bracket(bracket, team_east):
         print()
 
 # Example usage:
-team_east = populate_dictionaries.to_bracket_vis()['east']
-temp = list(populate_dictionaries.to_bracket_vis()['east'].keys())
-bracket = generate_bracket(list(populate_dictionaries.to_bracket_vis()['east'].keys()))
-print_bracket(bracket, team_east)
-for i in range (0,2):
-    new_teams = []
-    for item in bracket:
-        for item2 in item:
-            if team_east[item2[0]] < team_east[item2[1]]:
-                new_teams.append(item2[1])
-            else:
-                new_teams.append(item2[0])
-    print("Here")
-    print_bracket(bracket, team_east)
-    bracket = generate_bracket(new_teams)
-    print_bracket(bracket, team_east)
+populate_dictionaries.run()
+team_east = populate_dictionaries.rounds
+size = 16
+default_value = 0
+my_list = [default_value] * size
+j = 15
+my_list[0] = team_east[0][0]
+my_list[1] = team_east[0][15]
+my_list[2] = team_east[0][7]
+my_list[3] = team_east[0][8]
+my_list[4] = team_east[0][4]
+my_list[5] = team_east[0][11]
+my_list[6] = team_east[0][3]
+my_list[7] = team_east[0][12]
+my_list[8] = team_east[0][5]
+my_list[9] = team_east[0][10]
+my_list[10] = team_east[0][2]
+my_list[11] = team_east[0][13]
+my_list[12] = team_east[0][6]
+my_list[13] = team_east[0][9]
+my_list[14] = team_east[0][1]
+my_list[15] = team_east[0][14]
+# for i in range(0, len(team_east[0])):
+#     my_list[i] = team_east[i]
+bracket = generate_bracket(my_list)
+winners = team_east[1]
+print_bracket(bracket, winners)
+bracket = generate_bracket(team_east[1])
+print_bracket(bracket, winners)
